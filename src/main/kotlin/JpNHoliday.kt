968ccb93df83ea6c762b41095eb569cd4f6c33f7
fumiya-kume <fumiya.kume@hotmail.com>
2023-02-03 20:37:17 +0900

    Add read date feature

--- src/main/kotlin/JpNHoliday.kt
+++ src/main/kotlin/JpNHoliday.kt
@@ -1,15 +1,33 @@
+
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import kotlinx.datetime.LocalDate
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.InputStreamReader
+
 
 object JpNHoliday {
 
     val file = javaClass.getResource("syukujitsu.csv")
-    val csvReader = csvReader().readAll(file.readText())
+    var b_reader = BufferedReader(InputStreamReader(FileInputStream(file.file), "Shift-JIS"))
+    val text = b_reader.readText()
+    val csvReader = csvReader().readAllWithHeader(text)
 
     fun isReady() :Boolean{
         return csvReader.isNotEmpty()
     }
 
-    fun isHoliday(year: Int, month:Int, day:Int){
-
+    fun isHoliday(year: Int, month:Int, day:Int):Boolean {
+        return csvReader.any {
+            // YYYY/MM/DD
+            val evetnDateString = it["国民の祝日・休日月日"]!!
+            val eventDate = evetnDateString.split('/').run {
+                LocalDate(year = this[0].toInt(), monthNumber = this[1].toInt(), dayOfMonth = this[2].toInt())
+            }
+            val target = LocalDate(year = year, monthNumber = month, dayOfMonth = day)
+            return eventDate == target
+        }
     }
+
+    private val name ="国民の祝日・休日名称"
 }
--- src/test/kotlin/JpNHolidayTest.kt
+++ src/test/kotlin/JpNHolidayTest.kt
@@ -3,7 +3,12 @@ import kotlin.test.assertTrue
 
 class JpNHolidayTest {
     @Test
-    fun `load csv file`(){
+    fun `load csv file`() {
         assertTrue(JpNHoliday.isReady())
+    }
+
+    @Test
+    fun `should 1955年1月1日 is holiday`() {
+        assertTrue(JpNHoliday.isHoliday(year = 1955, month = 1, day = 1))
     }
 }
