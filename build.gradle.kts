e063212f5e4c452fdad3b794c7511299f1374be0
fumiya-kume <fumiya.kume@hotmail.com>
2023-02-07 22:17:50 +0900

    Add config

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,93 +35,147 @@ tasks.named<DokkaTask>("dokkaJavadoc") {
     outputDirectory.set(File(buildDir, "docs/javadoc"))
 }
 
-tasks.create("javadocJar", Jar::class) {
-    dependsOn("dokkaJavadoc")
-    archiveClassifier.set("javadoc")
-    from(File(buildDir, "docs/javadoc"))
+//tasks.create("javadocJar", Jar::class) {
+//    dependsOn("dokkaJavadoc")
+//    archiveClassifier.set("javadoc")
+//    from(File(buildDir, "docs/javadoc"))
+//}
+
+//tasks.create("sourcesJar", Jar::class) {
+//    dependsOn("classes")
+//    archiveClassifier.set("sources")
+//    from(sourceSets["main"].allSource)
+//}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
-
-tasks.create("sourcesJar", Jar::class) {
-    dependsOn("classes")
-    archiveClassifier.set("sources")
-    from(sourceSets["main"].allSource)
-}
+//publishing {
+//    repositories {
+//        maven {
+//            name = "OSSRH"
+//            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+//            credentials {
+//                username = System.getenv("MAVEN_USERNAME")
+//                password = System.getenv("MAVEN_PASSWORD")
+//            }
+//        }
+//    }
+//    publications {
+////        create<MavenPublication>("bintray") {
+//            create<MavenPublication>("mavenJava") {
+//                from(components["java"])
+//                pom{
+//                    artifact("$buildDir/outputs/aar/${base.archivesName}-release.aar")
+////                    artifact(tasks["sourcesJar"])
+////                    artifact(tasks["javadocJar"])
+//                    artifact(tasks["sourcesJar"])
+//                    name.set("kuu")
+//                    description.set("description")
+//                    url.set("url")
+//                    licenses{
+//                        license{
+//                            name.set("The Apache License, Version 2.0")
+//                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+//                            distribution.set("repo")
+//                        }
+//                    }
+//                    withXml {
+//                        val node = asNode()
+//                        node.appendNode("developers").appendNode("developer").apply {
+//                            appendNode("id", ProjectProperties.developerId)
+//                            appendNode("name", ProjectProperties.developerName)
+//                        }
+//                        node.appendNode("scm").apply {
+//                            appendNode("connection", ProjectProperties.Url.scm)
+//                            appendNode("developerConnection", ProjectProperties.Url.scm)
+//                            appendNode("url", ProjectProperties.Url.github)
+//                        }
+////                        val dependencies = node.appendNode("dependencies")
+////                        configurations.api.get().dependencies.forEach {
+////                            appendDependency(
+////                                dependencies,
+////                                groupId = it.group ?: "",
+////                                artifactId = it.name,
+////                                version = it.version ?: "",
+////                                scope = "compile"
+////                            )
+////                        }
+//                    }
+//                }
+//                groupId = ProjectProperties.groupId
+//                artifactId = base.archivesName.get()
+//                version = ProjectProperties.versionName
+////            }
+//        }
+//        repositories {
+//            maven {
+//                url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+//                credentials {
+//                    username = project.findProperty("sonatype_username") as? String ?: ""
+//                    password = project.findProperty("sonatype_password") as? String ?: ""
+//                }
+//            }
+//        }
+//    }
+//}
 
 publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "JpNHolidayK"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("My Library")
+                description.set("A concise description of my library")
+                url.set("http://www.example.com/library")
+                properties.set(mapOf(
+                    "myProp" to "value",
+                    "prop.with.dots" to "anotherValue"
+                ))
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("johnd")
+                        name.set("John Doe")
+                        email.set("john.doe@example.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://example.com/my-library.git")
+                    developerConnection.set("scm:git:ssh://example.com/my-library.git")
+                    url.set("http://example.com/my-library/")
+                }
+            }
+        }
+    }
     repositories {
         maven {
-            name = "OSSRH"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
-            }
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri(layout.buildDirectory.dir("repos/releases"))
+            val snapshotsRepoUrl = uri(layout.buildDirectory.dir("repos/snapshots"))
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
         }
     }
-    publications {
-        create<MavenPublication>("bintray") {
-            create<MavenPublication>("mavenJava") {
-                artifact("$buildDir/outputs/aar/${base.archivesName}-release.aar")
-                artifact(tasks["sourcesJar"])
-                artifact(tasks["javadocJar"])
-                groupId = ProjectProperties.groupId
-                artifactId = base.archivesName.get()
-                version = ProjectProperties.versionName
-                pom.withXml {
-                    val node = asNode()
-                    node.appendNode("name", ProjectProperties.name)
-                    node.appendNode("description", ProjectProperties.description)
-                    node.appendNode("url", ProjectProperties.Url.site)
-                    node.appendNode("licenses").appendNode("license").apply {
-                        appendNode("name", "The Apache License, Version 2.0")
-                        appendNode("url", "http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        appendNode("distribution", "repo")
-                    }
-                    node.appendNode("developers").appendNode("developer").apply {
-                        appendNode("id", ProjectProperties.developerId)
-                        appendNode("name", ProjectProperties.developerName)
-                    }
-                    node.appendNode("scm").apply {
-                        appendNode("connection", ProjectProperties.Url.scm)
-                        appendNode("developerConnection", ProjectProperties.Url.scm)
-                        appendNode("url", ProjectProperties.Url.github)
-                    }
-                    val dependencies = node.appendNode("dependencies")
-                    configurations.api.get().dependencies.forEach {
-                        appendDependency(
-                            dependencies,
-                            groupId = it.group ?: "",
-                            artifactId = it.name,
-                            version = it.version ?: "",
-                            scope = "compile"
-                        )
-                    }
-                    configurations.implementation.get().dependencies.forEach {
-                        appendDependency(
-                            dependencies,
-                            groupId = it.group ?: "",
-                            artifactId = it.name,
-                            version = it.version ?: "",
-                            scope = "runtime"
-                        )
-                    }
-                }
-            }
-        }
-        repositories {
-            maven {
-                url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-                credentials {
-                    username = project.findProperty("sonatype_username") as? String ?: ""
-                    password = project.findProperty("sonatype_password") as? String ?: ""
-                }
-            }
-        }
-        signing {
-            sign(publishing.publications["mavenJava"])
-        }
-    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
 fun appendDependency(
     parentNode: groovy.util.Node,
