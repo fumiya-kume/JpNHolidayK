8aa7eb6ed5bde6e0a7ee200562477b616bd760c7
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2023-02-12 21:14:04 +0900

    Setup Maven deploy config (#1)
    * Remove application plugin
    
    * Add java library plugin
    
    * Add signing plugin
    
    * Add source jar config
    
    * Add Dokka config
    
    * Add Maven central config
    
    * Change library version
    
    * Add publish workflow
    
    * Add login with environment variable
    
    * Fix type miss match
    
    * Update deploy CI
    
    * rename job
    
    * Add config
    
    * Update config
    
    * Update Github Actions secret config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
     kotlin("jvm") version "1.8.0"
-    application
+    `java-library`
+    `maven-publish`
+    signing
+    id("org.jetbrains.dokka") version "1.7.20"
 }
 
 group = "systems.kuu"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -17,10 +22,104 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+
 tasks.test {
     useJUnitPlatform()
 }
 
 kotlin {
     jvmToolchain(8)
+}
+
+tasks.named<DokkaTask>("dokkaJavadoc") {
+    outputDirectory.set(File(buildDir, "docs/javadoc"))
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            repositories {
+                maven {
+                    url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    credentials {
+                        username = project.findProperty("sonatype_username") as? String ?: ""
+                        password = project.findProperty("sonatype_password") as? String ?: ""
+                    }
+                }
+            }
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("JpNHolidayK")
+                description.set("A concise description of my library")
+                packaging = "jar"
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("kuu")
+                        name.set("Kuu")
+                        email.set("fumiya.kume@hotmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:fumiya-kume/JpNHolidayK.git")
+                    developerConnection.set("scm:git:ssh://github.com:fumiya-kume/JpNHolidayK.git")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+fun appendDependency(
+    parentNode: groovy.util.Node,
+    groupId: String,
+    artifactId: String,
+    version: String,
+    scope: String
+) {
+    parentNode.appendNode("dependency").apply {
+        appendNode("groupId", groupId)
+        appendNode("artifactId", artifactId)
+        appendNode("version", version)
+        appendNode("scope", scope)
+    }
+}
+
+object ProjectProperties {
+    const val groupId: String = "systems.kuu"
+    const val name: String = "JpNHolidayK"
+    const val description: String = "This library helps Japan National holiday"
+    const val developerId: String = "kuu"
+    const val developerName: String = "Kume Fumiya"
+
+    private const val versionMajor: Int = 0
+    private const val versionMinor: Int = 0
+    private const val versionPatch: Int = 1
+    const val versionName: String = "$versionMajor.$versionMinor.$versionPatch"
+
+    object Url {
+        const val site: String = "https://github.com/fumiya-kume/JpNHolidayK"
+        const val github: String = "https://github.com/fumiya-kume/JpNHolidayK"
+        const val scm: String = "scm:git@github.com:fumiya-kume/JpNHolidayK.git"
+    }
 }
