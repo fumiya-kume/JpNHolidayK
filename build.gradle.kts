0eb1a722b478cb4faafaa0e84000a37a11c9f305
fumiya-kume <fumiya.kume@hotmail.com>
2023-02-16 21:48:46 +0900

    Apply for sub project

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,20 @@ java {
     withSourcesJar()
 }
 
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
+
+    repositories {
+        // Required to download KtLint
+        mavenCentral()
+    }
+
+    // Optionally configure plugin
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+    }
+}
+
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
