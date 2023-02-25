be12713dc557f13c019856f3c9383a7f8d5eb7b1
fumiya-kume <fumiya.kume@hotmail.com>
2023-02-25 14:26:55 +0900

    Add sample method calling

--- sample/src/main/java/systems/kuu/Main.kt
+++ sample/src/main/java/systems/kuu/Main.kt
@@ -1,7 +1,14 @@
 package systems.kuu
 
+import HolidayRepository
+import JpNHoliday
+
 object Main {
     fun main() {
         println("Hello world!")
+    }
+
+    fun getNewYearHoliday(): String? {
+        return JpNHoliday(HolidayRepository.getInstance()).getNationalHolidayName(1955, 1, 1)
     }
 }
